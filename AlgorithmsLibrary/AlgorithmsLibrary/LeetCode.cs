04f4721310e7c1c093bc26538dda205180659766
Spencer <Spencer@SPENCER>
2020-06-14 15:58:41 -0700

    Add files

---
+++ AlgorithmsLibrary/AlgorithmsLibrary/LeetCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace AlgorithmsLibrary
+{
+    public class LeetCode
+    {
+        // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+        // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        public static int[] TwoSum(int[] integerArray, int desiredSum)
+        {
+            for (int i = 0; i < integerArray.Length-1; i++)
+            {
+                for (int e = i+1; e < integerArray.Length; e++)
+                {
+                    if (integerArray[i]+integerArray[e] == desiredSum)
+                    {
+                        int[] targetIndices = { i, e};
+                        return  targetIndices;
+                    }
+                }
+            }
+
+            throw new ArgumentException("The supplied integer array does not have 2 integers that add to the desired sum");
+        }
+    }
+}
---
+++ AlgorithmsLibrary/AlgorithmsLibraryTests/LeetCodeTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsLibrary.Tests
+{
+    [TestClass()]
+    public class LeetCodeTests
+    {
+        [TestMethod()]
+        public void TwoSumTestWithValidInputs()
+        {
+            int[] integerArray = new int[] { 2, 7, 1, 15 };
+            int targetSum = 9;
+
+            int[] indices = new int[2];
+            try
+            {
+                indices = LeetCode.TwoSum(integerArray, targetSum);
+            }
+            catch (ArgumentException)
+            {
+                Assert.Fail();
+            }
+
+            Assert.AreEqual(integerArray[indices[0]] + integerArray[indices[1]], targetSum);
+        }
+
+        [TestMethod()]
+        public void TwoSumTestWithBadInputs()
+        {
+            int[] integerArray = new int[] { 2, 10, 1, 15 };
+            int targetSum = 9;
+
+            int[] indices = new int[2];
+            bool caughtExceptionBool = false;
+            try
+            {
+                indices = LeetCode.TwoSum(integerArray, targetSum);
+            }
+            catch (Exception)
+            {
+                caughtExceptionBool = true;
+            }
+
+            Assert.IsTrue(caughtExceptionBool);
+        }
+    }
+}
